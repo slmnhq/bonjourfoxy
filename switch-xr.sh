743f9ad01fe8616f2d111c9f8308182f13abcd89
andrew@tj.id.au <andrew@tj.id.au>
2009-12-13 14:05:10 +0700

    Take a Firefox and XR SDK release and extract the SDK's to the appropriate directories

--- switch-xr.sh
+++ switch-xr.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
-rel=${1}
-archivedir=~/Projects/bonjourfoxy/sdk/archive
-outbase=~/Projects/bonjourfoxy/sdk
-pre=xulrunner-${rel}.en-US
-win32=${pre}.win32.sdk.zip
-win32dir=${outbase}/win32
-linux=${pre}.linux-i686.sdk.tar.bz2
-linuxdir=${outbase}/linux
-macppc=${pre}.mac-powerpc.sdk.tar.bz2
-macppcdir=${outbase}/macppc
-macintel=${pre}.mac-i386.sdk.tar.bz2
-macinteldir=${outbase}/macintel
-if [ ! $1 ]; then
-    echo "No release provided"
-    exit 1
-elif [ `ls ${archivedir}/{${win32},${linux},${macppc},${macintel}} | wc -l` -ne 4 ]; then
-    echo "Check version supplied"
-    exit 1
+function exiterror()  {
+    echo ${2}
+    exit ${1}
+}
+if [ $# -ne 2 ]; then
+    exiterror 1 "Usage: $0 [firefox_rel] [xulrunnersdk_rel]"
 fi
-echo ${win32} -\> ${win32dir}
-mkdir -p ${win32dir}
-unzip -q -o ${archivedir}/${win32} -d ${win32dir}
-echo ${linux} -\> ${linuxdir}
-mkdir -p ${linuxdir}
-tar -xjf ${archivedir}/${linux} -C ${linuxdir}
-echo ${macppc} -\> ${macppcdir}
-mkdir -p ${macppcdir}
-tar -xjf ${archivedir}/${macppc} -C ${macppcdir}
-echo ${macintel} -\> ${macinteldir}
-mkdir -p ${macinteldir}
-tar -xjf ${archivedir}/${macintel} -C ${macinteldir}
+ff_rel=${1}
+sdk_rel=${2}
+basedir=`dirname $0`
+archivedir=${basedir}/sdk/archive
+outbase=${basedir}/sdk/${ff_rel}
+pre=xulrunner-${sdk_rel}.en-US
+platforms="win32 linux macppc maci386"
+win32=${pre}.win32.sdk.zip
+linux=${pre}.linux-i686.sdk.tar.bz2
+macppc=${pre}.mac-powerpc.sdk.tar.bz2
+maci386=${pre}.mac-i386.sdk.tar.bz2
+for platform in ${platforms}; do
+    if [ ! -f ${archivedir}/${!platform} ]; then
+        exiterror 1 "Archive ${archivedir}/${!platform} missing"
+    fi
+done
+for platform in ${platforms}; do
+    platformdir=${outbase}/${platform}
+    echo ${archivedir}/${!platform} -\> ${platformdir}
+    mkdir -p ${platformdir}
+    if [ "$platform" == "win32" ]; then
+        cmd="unzip -q -o ${archivedir}/${!platform} -d ${platformdir}"
+    else
+        cmd="tar -xjf ${archivedir}/${!platform} -C ${platformdir}"
+    fi
+    ${cmd}
+    exitcode=$?
+    if [ ${exitcode} -ne 0 ]; then
+        exiterror ${exitcode} "Failed to extract ${archivedir}/${!platform} (${exitcode})"
+    fi
+done
